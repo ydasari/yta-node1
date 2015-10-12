dcb3aa61d25789788920e4f2ba2f301d17f8b13a
unknown <ydasari1@LH7U0CND535737T.ms.ds.uhc.com>
2015-10-12 11:26:57 -0400

    added localstorage message for login

--- source/js/controllers/loginPageCtrl.js
+++ source/js/controllers/loginPageCtrl.js
@@ -1,11 +1,19 @@
-app.controller('loginPageCtrl', ['$scope', 'authFactory', '$location', function($scope, authFactory, $location){
+app.controller('loginPageCtrl', ['$scope', 'authFactory', '$location', '$localStorage', function($scope, authFactory, $location, $localStorage){
+	
+	if($localStorage.successMsg==true){
+		$scope.successMsg = true;
+	}
+
 	$scope.checkLoginDetails = function(){
+		delete $localStorage.successMsg;
+		$scope.successMsg ="";
 		console.log("the user details are:", $scope.userEmail+ " "+$scope.userPassword);
 		authFactory.checkUser($scope.userEmail, $scope.userPassword).then(function(data){
 			if(data=="right credentials"){
 				$location.path('/userHomePage');
 			}
 			else{
+				$scope.loginFailure = true; 
 				console.log("Email or password! incorrect! Please enter user@gmail.com as username and password as password");
 			}
 		});
